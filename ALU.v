bc47ea52437069f8a89d9e8c3b7f9137c777f1ca
Pranav Sapkale <pransomit@gmail.com>
2026-07-13 19:07:24 +0530

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -25,22 +25,26 @@ input [31:0] data1,
 input [31:0] data2,
 input [3:0] operation,
 output reg [31:0] result,
-output zero
+output zero,
+output alu_lt,
+output alu_ltu
     );
     always @ (*) begin
         case (operation) 
-            4'b0000: result <= data1 & data2;
-            4'b0001: result <= data1 | data2;
-            4'b0010: result <= data1 + data2;
-            4'b0110: result <= data1 - data2;
-            4'b0100: result <= data1 ^ data2;
-            4'b0101: result <= data1 >> data2[4:0];
-            4'b1101: result <= $signed(data1) >>> data2[4:0];
-            4'b0011: result <= data1 << data2[4:0];
-            4'b0111: result <= $signed(data1) < $signed(data2) ? 1 : 0;
-            4'b1000: result <= data1 < data2 ? 1 : 0;
-            default: result <= 32'b0;
+            4'b0000: result = data1 & data2;
+            4'b0001: result = data1 | data2;
+            4'b0010: result = data1 + data2;
+            4'b0110: result = data1 - data2;
+            4'b0100: result = data1 ^ data2;
+            4'b0101: result = data1 >> data2[4:0];
+            4'b1101: result = $signed(data1) >>> data2[4:0];
+            4'b0011: result = data1 << data2[4:0];
+            4'b0111: result = $signed(data1) < $signed(data2) ? 1 : 0;
+            4'b1000: result = data1 < data2 ? 1 : 0;
+            default: result = 32'b0;
         endcase
     end
     assign zero = (result == 32'b0) ? 1 : 0;
+assign alu_lt  = ($signed(data1) < $signed(data2))  ? 1'b1 : 1'b0;
+assign alu_ltu = (data1 < data2)                     ? 1'b1 : 1'b0;
 endmodule
